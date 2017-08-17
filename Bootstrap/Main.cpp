c8a37316f265e1a422375db8e811661a343e3792
Christopher Walle <cjwalle97@gmail.com>
2017-08-17 17:38:50 -0500

    Update

--- Bootstrap/Main.cpp
+++ Bootstrap/Main.cpp
@@ -24,6 +24,7 @@ int main()
 	Gizmos::create();
 
 	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
 
 	while (true)
 	{
@@ -34,6 +35,7 @@ int main()
 		}
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE))
 		{
+			Gizmos::destroy();
 			glfwSetWindowShouldClose(window, true);
 		}
 	}
